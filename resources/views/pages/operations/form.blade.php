b0d24a16b66000ac7e422d7d5943a9e88079408d
FamoryGeek <famory9301@gmail.com>
2024-01-24 19:39:27 +0000

    template

---
+++ resources/views/pages/operations/form.blade.php
@@ -0,0 +1,10 @@
+@extends('partials.interface')
+@section('content')
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Faire une operation</h1>
+    </div>
+    <!-- Content Row -->
+    <div class="row">
+    </div>
+@endsection
